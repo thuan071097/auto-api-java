6ca1fadde307b5deea8565496aa1661de40d47f5
thuan071097 <nguyenthuan.071097@gmail.com>
2024-08-20 19:02:30 +0700

    [ID-124] Init RestAssured test and library

--- src/test/java/api/test/FirstApiTest.java
+++ src/test/java/api/test/FirstApiTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class FirstApiTest {
     @Test
-    void testApi (){
+    void testGetApi (){
         System.out.println("Hello");
+        String a = "Tho";
+        a = a.concat("Test");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
